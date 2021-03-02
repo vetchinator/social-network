467e4ce989b0ce069fd5cc50050150b38f82626f
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-02 15:23:41 +0300

    Change post props

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.post}>
       <img
         src="https://www.iconsdb.com/icons/preview/orange/user-xxl.png"
         alt="logo"
       />
-      <p>Some post </p>
+      <div className={s.comment}>
+        <p>{props.message}</p>
+        <p>like <span>{props.countLike}</span></p>
+        
+      </div>
     </div>
   );
 };
