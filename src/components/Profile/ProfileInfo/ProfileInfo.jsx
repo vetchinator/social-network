03005bb3897852a09638e76af3a980e0328b34eb
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-03 18:48:02 +0300

    Add ProfileInfo component

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from "./ProfileInfo.module.css";
+
+const ProfileInfo = () => {
+  return (
+    <div className={s.profileInfo}>
+      <img
+        src="https://icocnews.ru/wp-content/uploads/2015/09/priroda.jpg"
+        alt=""
+      />
+      <div>ava + description</div>
+    </div>
+  );
+};
+
+export default ProfileInfo;
