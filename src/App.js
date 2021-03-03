431b391b4ad98579b873dcf4c00cb32146e21f36
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-03 14:25:51 +0300

    Add route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 
 //Components
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialog from "./components/Dialog/Dialog";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
-    <div className="container">
-      <Header />
-      <Navbar />
-      <div className="body__wrapper">
-        <Profile />
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <Navbar />
+        <div className="body__wrapper">
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialog" component={Dialog} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
