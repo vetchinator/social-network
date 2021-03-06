772ffc8bd68a30a27f9a62f5d9f1d54a96092134
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-06 14:25:43 +0300

    Split component Dialog

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,43 +1,20 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import s from "./Dialog.module.css";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-const DialogItem = (props) => {
-  let path = "/dialog/" + props.id;
+const Dialog = (props) => {
+  let dialogElements = props.dialogs.map((d) => (
+    <DialogItem id={d.id} name={d.name} />
+  ));
+  let messageElements = props.messages.map((m) => (
+    <Message message={m.message} />
+  ));
 
-  return (
-    <NavLink to={path} activeClassName={s.item + " " + s.active}>
-      <div className={s.item}>
-        <img
-          src="https://www.iconsdb.com/icons/preview/orange/user-xxl.png"
-          alt="avatar"
-        />
-        <p>{props.name}</p>
-      </div>
-    </NavLink>
-  );
-};
-
-const Message = (props) => {
-  return <div className={s.message}>{props.message}</div>;
-};
-
-const Dialog = () => {
   return (
     <div className={s.dialog}>
-      <div className={s.list}>
-        <DialogItem id="1" name="Sasha" />
-        <DialogItem id="2" name="Oleg" />
-        <DialogItem id="3" name="Aleksej" />
-        <DialogItem id="4" name="Svetlana" />
-        <DialogItem id="5" name="Nastya" />
-      </div>
-      <div className={s.messages}>
-        <Message message="Hi!" />
-        <Message message="Hi!" />
-        <Message message="How are you?" />
-        <Message message="I'm fine, thank you!" />
-      </div>
+      <div className={s.list}>{dialogElements}</div>
+      <div className={s.messages}>{messageElements}</div>
     </div>
   );
 };
---
+++ src/components/Dialog/DialogItem/DialogItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./../Dialog.module.css";
+
+const DialogItem = (props) => {
+  let path = "/dialog/" + props.id;
+
+  return (
+    <NavLink to={path} activeClassName={s.item + " " + s.active}>
+      <div className={s.item}>
+        <img
+          src="https://www.iconsdb.com/icons/preview/orange/user-xxl.png"
+          alt="avatar"
+        />
+        <p>{props.name}</p>
+      </div>
+    </NavLink>
+  );
+};
+
+export default DialogItem;
