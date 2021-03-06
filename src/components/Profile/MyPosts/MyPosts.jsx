a5c79a7e5b749eccef55ffc3b8fc686644b64ca3
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-06 14:26:49 +0300

    Add props and map elements

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
+  let postElements = props.posts.map((p) => (
+    <Post message={p.message} countLike={p.likesCount} />
+  ));
+
   return (
     <div>
       <h2 className={s.title}>My Posts</h2>
@@ -14,8 +18,7 @@ const MyPosts = () => {
           <button>Add Post</button>
         </div>
       </div>
-      <Post message="Hi, are you doing?" countLike="15" />
-      <Post message="Hi, i'm learning React" countLike="20" />
+      {postElements}
     </div>
   );
 };
