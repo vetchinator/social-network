1858e8c8f04c7cfb376085636cb004590ac710e1
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-09 18:23:47 +0300

    Add updateNewPostText

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,9 +6,12 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
 
   let addNewPost = () => {
+    props.addPost();
+  };
+
+  let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.addPost(text);
-    newPostElement.current.value = '';
+    props.updateNewPostText(text);
   };
 
   let postElements = props.posts.map((p) => (
@@ -20,7 +23,11 @@ const MyPosts = (props) => {
       <h2 className={s.title}>My Posts</h2>
       <div className={s.newPost}>
         <div>
-          <textarea ref={newPostElement}></textarea>
+          <textarea
+            ref={newPostElement}
+            onChange={onPostChange}
+            value={props.newPostText}
+          ></textarea>
         </div>
         <div>
           <button onClick={addNewPost}>Add Post</button>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,11 +4,15 @@ import s from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = (props) => {
-
   return (
     <div className={s.profile}>
       <ProfileInfo />
-      <MyPosts posts={props.state.posts}  addPost={props.addPost} />
+      <MyPosts
+        posts={props.profilePage.posts}
+        addPost={props.addPost}
+        updateNewPostText={props.updateNewPostText}
+        newPostText={props.profilePage.newPostText}
+      />
     </div>
   );
 };
