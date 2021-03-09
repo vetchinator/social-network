5bf9292f060ec934de9abc6ac659bf1229250415
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-09 18:21:35 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import state, { addPost } from "./redux/state";
+import state from "./redux/state";
 import { rerenderEntireTree } from "./render";
 import reportWebVitals from "./reportWebVitals";
 
 
 rerenderEntireTree(state);
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
