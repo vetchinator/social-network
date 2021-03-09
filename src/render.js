8142ba81f3bac87d95af62e27cfc0a8ef0176ce4
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-09 18:23:04 +0300

    Add props(update) to App

--- src/render.js
+++ src/render.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { addMessage, addPost } from "./redux/state";
+import { addMessage, addPost, updateNewMessageText, updateNewPostText } from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
 // addPost('React it\'s cool');
@@ -10,10 +10,15 @@ export let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} addPost={addPost}  addMessage={addMessage}/>
+        <App
+          state={state}
+          addPost={addPost}
+          addMessage={addMessage}
+          updateNewPostText={updateNewPostText}
+          updateNewMessageText={updateNewMessageText}
+        />
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
   );
-}
-
+};
