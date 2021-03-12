451f6df9fb8bd8e7aa14b474fe061ef819c5b16f
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-12 15:09:26 +0300

    Refactoring reducers

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -26,17 +26,17 @@ const dialogsReducer = (state = initialState, action) => {
                 id: 5,
                 message: state.newMessageText,
             };
-            let stateCopy = { ...state };
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.newMessageText = "";
-            return stateCopy;
+            return {
+                ...state,
+                messages: [...state.messages, newMessage],
+                newMessageText: "",
+            };
         }
 
         case UPDATE_NEW_MESSAGE_BODY: {
             let stateCopy = { ...state };
             stateCopy.newMessageText = action.newText;
-            return stateCopy;
+            return { ...state, newMessageText: action.newText };
         }
         default:
             return state;
