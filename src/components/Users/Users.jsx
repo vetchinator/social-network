a5e168a0336b73ecb545fe43f8d7030a8b94d0c9
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-14 15:35:51 +0300

    Change functional on class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,98 +1,54 @@
 import React from "react";
 import s from "./Users.module.css";
+import * as axios from 'axios';
+import userPhoto from './../../assets/images/user.png';
 
-const Users = (props) => {
-    
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                followed: true,
-                avatarSrc:
-                    "https://smashinglogo.com/static/img/virtual-designers/peter.gif",
-                fullName: "Aleksej Smirnov",
-                location: { country: "Russia", city: "Kursk" },
-            },
-            {
-                id: 2,
-                followed: false,
-                avatarSrc:
-                    "https://smashinglogo.com/static/img/virtual-designers/peter.gif",
-                fullName: "Nastya Sokolova",
-                location: { country: "Russia", city: "Moskow" },
-            },
-            {
-                id: 3,
-                followed: true,
-                avatarSrc:
-                    "https://smashinglogo.com/static/img/virtual-designers/peter.gif",
-                fullName: "Evgenij Starkov",
-                location: { country: "Russia", city: "Volgograd" },
-            },
-            {
-                id: 4,
-                followed: true,
-                avatarSrc:
-                    "https://smashinglogo.com/static/img/virtual-designers/peter.gif",
-                fullName: "Sasha Alekseev",
-                location: { country: "Russia", city: "Kazan" },
-            },
-            {
-                id: 5,
-                followed: false,
-                avatarSrc:
-                    "https://smashinglogo.com/static/img/virtual-designers/peter.gif",
-                fullName: "Sveta Pupova",
-                location: { country: "Russia", city: "Belgorod" },
-            },
-            {
-                id: 6,
-                followed: false,
-                avatarSrc:
-                    "https://smashinglogo.com/static/img/virtual-designers/peter.gif",
-                fullName: "Stas Vetkin",
-                location: { country: "USA", city: "New York" },
-            },
-        ]);
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+            props.setUsers(response.data.items);
+        })
     }
-
-
-    return (
-        <div className={s.users}>
-            {props.users.map((user) => (
-                <div className={s.user} key={user.id}>
-                    <div>
-                        <img
-                            className={s.avatar}
-                            src={user.avatarSrc}
-                            alt="avatar"
-                        />
+    
+    render() {
+        return (
+            <div className={s.users}>
+                {this.props.users.map((user) => (
+                    <div className={s.user} key={user.id}>
+                        <div>
+                            <img
+                                className={s.avatar}
+                                src={user.photos.small ? user.photos.small : userPhoto}
+                                alt="avatar"
+                            />
+                        </div>
+                        <div>{user.name}</div>
+                        <div>{'user.location.country'}</div>
+                        <div>{'user.location.city'}</div>
+                        {user.followed ? (
+                            <button
+                                onClick={() => {
+                                    this.props.unfollow(user.id);
+                                }}
+                            >
+                                Unfollow
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() => {
+                                    this.props.follow(user.id);
+                                }}
+                            >
+                                Follow
+                            </button>
+                        )}
                     </div>
-                    <div>{user.fullName}</div>
-                    <div>{user.location.country}</div>
-                    <div>{user.location.city}</div>
-                    {user.followed ? (
-                        <button
-                            onClick={() => {
-                                props.unfollow(user.id);
-                            }}
-                        >
-                            Unfollow
-                        </button>
-                    ) : (
-                        <button
-                            onClick={() => {
-                                props.follow(user.id);
-                            }}
-                        >
-                            Follow
-                        </button>
-                    )}
-                </div>
-            ))}
-        </div>
-    );
-    // <div className={s.users}>{usersElements}</div>;
-};
+                ))}
+            </div>
+        );
+    }
+}
 
 export default Users;
