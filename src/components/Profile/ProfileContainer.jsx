41d079def5a843d5259903de6c93e6fd4e0b4d69
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-03-19 16:03:56 +0300

    fix

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -7,7 +7,6 @@ import { withRouter } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
-        debugger
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = 2;
