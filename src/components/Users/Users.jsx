fce08b8fa1bb1c7d93eb44bdcd726da96083a886
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-04-15 13:18:10 +0300

    Change inside components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,63 +1,21 @@
 import React from "react";
 import s from "./Users.module.css";
-import userPhoto from "./../../assets/images/user.png";
-import { NavLink } from "react-router-dom";
+import Paginator from "../common/Paginator/Paginator";
+import User from './User/User';
 
 const Users = (props) => {
-    let pageCount = Math.ceil(props.totalCountUsers / props.pageSize);
-    let pages = [];
-    for (let i = 1; i <= pageCount; i++) {
-        pages.push(i);
-    }
-
     return (
         <div>
-            <div>
-                {pages.map((page) => (
-                    <span
-                        key={page}
-                        className={props.currentPage === page ? s.selectedPage : ""}
-                        onClick={() => props.onPageChanged(page)}
-                    >
-                        {page}
-                    </span>
-                ))}
-            </div>
+            <Paginator
+                totalCountUsers={props.totalCountUsers}
+                pageSize={props.pageSize}
+                currentPage={props.currentPage}
+                onPageChanged={props.onPageChanged}
+            />
             <div className={s.users}>
                 {props.users.map((user) => (
-                    <div className={s.user} key={user.id}>
-                        <div>
-                            <NavLink to={"/profile/" + user.id}>
-                                <img
-                                    className={s.avatar}
-                                    src={user.photos.small ? user.photos.small : userPhoto}
-                                    alt="avatar"
-                                />
-                            </NavLink>
-                        </div>
-                        <div>{user.name}</div>
-                        <div>{"user.location.country"}</div>
-                        <div>{"user.location.city"}</div>
-                        {user.followed ? (
-                            <button
-                                disabled={props.followingInProgress.some((id) => id === user.id)}
-                                onClick={() => {
-                                    props.unfollow(user.id);
-                                }}
-                            >
-                                Unfollow
-                            </button>
-                        ) : (
-                            <button
-                                disabled={props.followingInProgress.some((id) => id === user.id)}
-                                onClick={() => {
-                                    props.follow(user.id);
-                                }}
-                            >
-                                Follow
-                            </button>
-                        )}
-                    </div>
+                    <User key={user.id} user={user} unfollow={props.unfollow} follow={props.follow}
+                    followingInProgress={props.followingInProgress} />
                 ))}
             </div>
         </div>
