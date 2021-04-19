af3adf00f07bae084bc5aa2880f12612297cee13
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-04-19 17:17:36 +0300

    Change paginator

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,16 +2,31 @@ import React from "react";
 import s from "./Users.module.css";
 import Paginator from "../common/Paginator/Paginator";
 import User from './User/User';
+// import ReactPaginate from 'react-paginate';
 
 const Users = (props) => {
     return (
         <div>
+            {/* <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={props.totalCountUsers / props.pageSize}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={props.onPageChanged}
+            containerClassName={s.pagination}
+            activeClassName={s.active}
+            initialPage={props.currentPage}
+            /> */}
             <Paginator
                 totalCountUsers={props.totalCountUsers}
                 pageSize={props.pageSize}
                 currentPage={props.currentPage}
                 onPageChanged={props.onPageChanged}
             />
+
             <div className={s.users}>
                 {props.users.map((user) => (
                     <User key={user.id} user={user} unfollow={props.unfollow} follow={props.follow}
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Paginator.module.css";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
-const Paginator = ({ totalCountUsers, pageSize, currentPage, onPageChanged }) => {
+const Paginator = ({ totalCountUsers, pageSize, currentPage, onPageChanged, portionSize = 15 }) => {
     let pageCount = Math.ceil(totalCountUsers / pageSize);
     let pages = [];
     for (let i = 1; i <= pageCount; i++) {
         pages.push(i);
     }
 
+    let [portionNumber, setPortionNumber] = useState(1);
+    let portionCount = Math.ceil(pageCount / portionSize);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;  
+
     return (
         <div>
             <div>
-                {pages.map((page) => (
+                {
+                    portionNumber > 1 && 
+                    <button onClick={()=> { setPortionNumber(portionNumber - 1) }}><ArrowBackIosIcon /></button>    
+                }
+                {pages
+                .filter(page => page >= leftPortionPageNumber && page <= rightPortionPageNumber)
+                .map((page) => (
                     <span
                         key={page}
                         className={currentPage === page ? s.selectedPage : ""}
@@ -20,6 +33,10 @@ const Paginator = ({ totalCountUsers, pageSize, currentPage, onPageChanged }) =>
                         {page}
                     </span>
                 ))}
+                {
+                    portionCount > portionNumber && 
+                    <button onClick={()=> { setPortionNumber(portionNumber + 1)} }><ArrowForwardIosIcon /></button>    
+                }
             </div>
         </div>
     );
