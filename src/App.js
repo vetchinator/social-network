fb96ed5a72f0f376c93caf4f26410785d7467385
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-04-19 17:36:49 +0300

    Add withSuspense

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
 import { BrowserRouter, Route, withRouter } from "react-router-dom";
 import "./App.css";
-import { initializeApp } from './redux/app-reducer';
+import { initializeApp } from "./redux/app-reducer";
 
 //Components
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogContainer from "./components/Dialog/DialogContainer";
+// import DialogContainer from "./components/Dialog/DialogContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+//import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import { compose } from "redux";
 import { connect, Provider } from "react-redux";
 import Preloader from "./components/common/Preloader/Preloader";
-import store from './redux/redux-store';
+import store from "./redux/redux-store";
+import { withSuspense } from "./hoc/withSuspense";
+
+let ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
+let DialogContainer = React.lazy(() => import("./components/Dialog/DialogContainer"));
 
 class App extends React.Component {
-
     componentDidMount() {
-        this.props.initializeApp();        
+        this.props.initializeApp();
     }
 
     render() {
-        
         if (!this.props.initialized) {
-            return <Preloader />
+            return <Preloader />;
         }
 
         return (
@@ -35,8 +37,14 @@ class App extends React.Component {
                 <HeaderContainer />
                 <Navbar />
                 <div className="body__wrapper">
-                    <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-                    <Route path="/dialog" render={() => <DialogContainer />} />
+                    <Route
+                        path="/profile/:userId?"
+                        render={withSuspense(ProfileContainer)}
+                    />
+                    <Route
+                        path="/dialog"
+                        render={withSuspense(DialogContainer)}
+                    />
                     <Route path="/news" render={() => <News />} />
                     <Route path="/music" render={() => <Music />} />
                     <Route path="/settings" render={() => <Settings />} />
@@ -46,23 +54,22 @@ class App extends React.Component {
             </div>
         );
     }
-};
+}
 
 const mapStateToProps = (state) => ({
-    initialized: state.app.initialized
-})
+    initialized: state.app.initialized,
+});
 
-let AppContainer = compose(
-    withRouter,
-    connect(mapStateToProps, {initializeApp})
-)(App);
+let AppContainer = compose(withRouter, connect(mapStateToProps, { initializeApp }))(App);
 
 const MainApp = (props) => {
-    return <BrowserRouter>
-        <Provider store={store}>
-            <AppContainer />
-        </Provider>
-    </BrowserRouter>;
+    return (
+        <BrowserRouter>
+            <Provider store={store}>
+                <AppContainer />
+            </Provider>
+        </BrowserRouter>
+    );
 };
 
 export default MainApp;
