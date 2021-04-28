ce35596f1b4bacca8962d0a6b0a3abe177415ace
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-04-28 13:33:06 +0300

    Fix path img

--- src/components/Dialog/DialogItem/DialogItem.jsx
+++ src/components/Dialog/DialogItem/DialogItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import s from "./../Dialog.module.css";
+import userLogo from "../../../assets/images/user.png";
 
 const DialogItem = (props) => {
     let path = "/dialog/" + props.id;
@@ -8,7 +9,7 @@ const DialogItem = (props) => {
     return (
         <NavLink to={path} activeClassName={s.item + " " + s.active}>
             <div className={s.item}>
-                <img src="/user.png" alt="avatar" />
+                <img src={userLogo} alt="user logo" />
                 <p>{props.name}</p>
             </div>
         </NavLink>
