7d941b3273909e99edbf0217ffc997716d03e2de
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-05-11 16:55:14 +0300

    Change redux-store on ts

---
+++ src/redux/redux-store.ts
@@ -0,0 +1,31 @@
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+import sidebarReducer from "./sidebar-reducer";
+import usersReducer from './users-reducer';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import authReducer from "./auth-reducer";
+import thunkMiddleware from 'redux-thunk';
+import appReducer from './app-reducer';
+
+
+let rootReducer = combineReducers({ 
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer,
+    usersPage: usersReducer,
+    auth: authReducer,
+    app: appReducer
+})
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+// @ts-ignore
+window.store = store;
+
+export default store;
