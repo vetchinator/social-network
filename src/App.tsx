177e41d00f703734d41f9243b598b5554022e3bc
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-05-31 18:57:08 +0300

    Change on functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { HashRouter, Redirect, Route, Switch, withRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { initializeApp } from "./redux/app-reducer";
 
@@ -10,92 +10,78 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import UsersPage from "./components/Users/UsersPage";
 import Login from "./components/Login/Login";
-import { compose } from "redux";
-import { connect, Provider } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import Preloader from "./components/common/Preloader/Preloader";
-import store, { RootState } from "./redux/redux-store";
+import store from "./redux/redux-store";
 import { withSuspense } from "./hoc/withSuspense";
-import ErrorPage from './components/ErrorPage/ErrorPage';
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 import { Header } from "./components/Header/Header";
+import { selectIsInitialized } from "./redux/selectors/app-selector";
 
 let ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
 let DialogContainer = React.lazy(() => import("./components/Dialog/Dialog"));
 
-let SuspensedProfile =  withSuspense(ProfileContainer);
-let SuspensedDialog =  withSuspense(DialogContainer);
+let SuspensedProfile = withSuspense(ProfileContainer);
+let SuspensedDialog = withSuspense(DialogContainer);
 
-type PropType = StatePropType & DispatchPropType;
+const App: React.FC = () => {
+    const initialized = useSelector(selectIsInitialized);
 
-type StatePropType = {
-    initialized: boolean
-}
-
-type DispatchPropType = {
-    initializeApp: () => void
-}
-
-class App extends React.Component<PropType> {
-
-    cacthAllUnhandledErrors(e: PromiseRejectionEvent) {
+    const dispatch = useDispatch();
+    const initializeAppHandler = () => {
+        dispatch(initializeApp());
+    };
+    const cacthAllUnhandledErrors = (e: PromiseRejectionEvent) => {
         alert("Some error occured");
-    }
+    };
 
-    componentDidMount() {
-        this.props.initializeApp();
-        window.addEventListener('unhandledrejection', this.cacthAllUnhandledErrors);
-    }
-    
-    componentWillUnmount() {
-        window.removeEventListener('unhandledrejection', this.cacthAllUnhandledErrors);
-    }
+    useEffect(() => {
+        initializeAppHandler();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    render() {
-        if (!this.props.initialized) {
-            return <Preloader />;
-        }
+    useEffect(() => {
+        window.addEventListener("unhandledrejection", cacthAllUnhandledErrors);
+        return () => {
+            window.removeEventListener("unhandledrejection", cacthAllUnhandledErrors);
+        };
+    }, []);
 
-        return (
-            <div className="container">
-                <Header />
-                <Navbar />
-                <div className="body__wrapper">
-                    <Switch>
-                        <Route
-                            exact
-                            path="/"
-                            render={() => {
-                                return <Redirect to="/profile" />;
-                            }}
-                        />
-                        <Route path="/profile/:userId?" render={() => <SuspensedProfile />} />
-                        <Route path="/dialog" render={() => <SuspensedDialog />} />
-                        <Route path="/news" render={() => <News />} />
-                        <Route path="/music" render={() => <Music />} />
-                        <Route path="/settings" render={() => <Settings />} />
-                        <Route path="/users" render={() => <UsersPage />} />
-                        <Route path="/login" render={() => <Login />} />
-                        <Route path="*" render={() => <ErrorPage />} />
-                    </Switch>
-                </div>
+    if (!initialized) {
+        return <Preloader />;
+    }
+    return (
+        <div className="container">
+            <Header />
+            <Navbar />
+            <div className="body__wrapper">
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => {
+                            return <Redirect to="/profile" />;
+                        }}
+                    />
+                    <Route path="/profile/:userId?" render={() => <SuspensedProfile />} />
+                    <Route path="/dialog" render={() => <SuspensedDialog />} />
+                    <Route path="/news" render={() => <News />} />
+                    <Route path="/music" render={() => <Music />} />
+                    <Route path="/settings" render={() => <Settings />} />
+                    <Route path="/users" render={() => <UsersPage />} />
+                    <Route path="/login" render={() => <Login />} />
+                    <Route path="*" render={() => <ErrorPage />} />
+                </Switch>
             </div>
-        );
-    }
-}
-
-const mapStateToProps = (state: RootState): StatePropType => ({
-    initialized: state.app.initialized,
-});
-
-let AppContainer = compose<React.ComponentType>(
-    withRouter,
-    connect<StatePropType, DispatchPropType, {}, RootState>(mapStateToProps, { initializeApp })
-)(App);
+        </div>
+    );
+};
 
 const MainApp: React.FC = () => {
     return (
         <HashRouter>
             <Provider store={store}>
-                <AppContainer />
+                <App />
             </Provider>
         </HashRouter>
     );
